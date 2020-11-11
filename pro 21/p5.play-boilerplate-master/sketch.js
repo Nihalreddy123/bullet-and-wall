f42317eb20cbbb110bbf03318917fb03fecaabc8
Nihalreddy123 <73288057+Nihalreddy123@users.noreply.github.com>
2020-11-11 19:57:46 +0530

    Add files via upload

---
+++ pro 21/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,62 @@
+var thickness,wall;
+var speed, weight; 
+
+function setup() {
+  createCanvas(1600, 400);
+  
+thickness=random(22,83);
+
+  speed=random(223,321);
+  weight=random(30,52);
+
+  car=createSprite(50, 200, 50,50);   
+  car.velocityX = speed;
+  car.shapeColor=color(255);
+
+  wall=createSprite(1500,200,thickness, height/2);
+	wall.shapeColor=color(80,80,80)
+}
+function draw() {
+  background(0);
+  
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+  	car.velocityX=0;
+  	var deformation=0.5 * weight * speed* speed/22500;
+	if(deformation>180)
+	{
+	car.shapeColor=color(255,0,0);
+	}
+
+	if(deformation<180 && deformation>100)
+	{
+		car.shapeColor=color(230,230,0);
+	}
+function hasCollided (lbullet,lwall)
+{
+bulletRightEdge= lBullet.x + lbullet.width;
+wallLeftEdge= lwall.x;
+if (bulletRightEdge>=wallLeftEdge){
+	return true
+}
+ return false 
+}
+
+if (hasCollided(bullet,wall))
+{
+bullet.velocityX=0;
+var damage=0.5 * wieght * speed * speed/(thickness * thickness * thickness);
+}
+
+if (damage>10)
+{
+	wall.shapeColor=color(255,0,0);
+}
+
+if(damage<10)
+{
+	wall.shapeColor=color(0,255,0);
+}
+}
+drawSprites();
+}
